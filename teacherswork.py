fa89728e660e2948b8d01da0165668e86cb2b213
Gintama1337 <gintamashi1337@gmail.com>
2022-01-09 17:23:48 +0500

    my first commit

--- teacherswork.py
+++ teacherswork.py
@@ -44,7 +44,7 @@ c = {'k':1, 'd':2, 'f':'3', 'g':4, 'h':5}
 
 #==//Работа с функциями//==
 a = 5
-def main(a, b):
+def main(a = 0, b = 0):
     if isinstance(a, str) or isinstance(b, str):
     # if a or b == str: #if a=1=True or b!=str=False
         return 'ты ввёл не то значение индюк'
@@ -52,7 +52,7 @@ def main(a, b):
         return (a + b) ** 2  # после return ничего не выполняется
 
     # print((a+b)**2)
-print(main(1, 6))
+# print(main(1, 2))
 # print(isinstance(5, str))
 # print(bool(5))
 # main(5, 6)
@@ -60,10 +60,20 @@ print(main(1, 6))
 #==//Работа с рекурсиями//==
 #В РЕКУРСИИ ОБЯЗАТЕЛЬНО ДОЛЖНО БЫТЬ УКАЗАННО УСЛОВИЕ ЗАВЕРШЕНИЯ РЕКУРСИИ.
 #n! = факториал = 1*2*3*...*n
-def factorial(n):
-    factorial(n)
-
-
+def factorial(n:int)->int:
+    """это функция считает факториал
+    пример:
+    4 * factorial(n-1=3)
+    3*factorial(n-1=2)
+    2*factorial(n-1=1)
+    1*factorial(n-1=0)
+    factorial(0) return 1
+    4*3 *2*1*1"""
+    if n == 0:
+        return 1
+    return n * factorial(n-1) # 4 * factorial(n-1=3) -> 12 *
+print(factorial(4))
+# ДЗ: СДЕЛАТЬ СВОЙ ФАКТОРИАЛ В ЦИКЛЕ
 
 # cc = 4
 # for i in range(1, c):
