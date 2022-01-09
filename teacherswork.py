aeee63c028907c99d6c30cce9a715079965e00a9
Gintama1337 <gintamashi1337@gmail.com>
2022-01-09 15:38:50 +0500

    my first commit

--- teacherswork.py
+++ teacherswork.py
@@ -1,6 +1,38 @@
-a = (1, 2, '3')
-a = tuple([1, 23, '3'])
+from random import randint
+#==//Работа с кортежами//==
+a = (1, 2, '3', 3)
+# print(a)
+# print(a.__sizeof__())
+
+#==//Работа с листом//==
+b = [1, 2, '3', 3]
+
+# for i in range(len(b)):
+#     b[i] = randint(0, 4)
+#     b[i] = int(b[i]) + 1
+b.append('mayonez')
+# print(b)
+# print(b.pop())
+# print(b)
+# for i in range(0, 5, 2): #ранжировка range(start, stop, step)
+#     print(i)
+# print(len(b))
+# print(range(len(b)))
+
+# print(b.__sizeof__())
+# print(b[1])
+
+#==//Работа со словарями//==
+
+c = {'k':1, 'd':2, 'f':'3', 'g':4, 'h':5}
+# for i in c.keys():
+#     c[i] = randint(0, 10)
+# print(c.items())
+# print(c['d'])
+# print(c.values())
+
+# print(c.__sizeof__())
+
+#==//Работа с функциями//==
 
 
-
-print(a[1])
