8db649616c6ba7264e8f4518e7e9d666e1100782
Gintama1337 <gintamashi1337@gmail.com>
2022-02-26 15:37:13 +0500

    add some fix to hw9

--- homework.py
+++ homework.py
@@ -137,60 +137,44 @@ def repetition_finder(test_string:str)->list:
 # print(repetition_finder(test_string))
 
 #HW9
-discr_result = []
-a = 1
-b = -2
-c = -24
+discr = lambda a, b, c : (b**2)-4*a*c
+
+quadratic_root = []
+
 def positive_result(function):
     def wrapper(*args, **kwargs):
-        result = function(*args,**kwargs)
+        result = function(*args, **kwargs)
         if result < 0:
             return "Уравнение не имеет корней"
-        elif result == 0:
-            x = (-b) / (2 * a)
-            discr_result.append(x)
-            return discr(a, b, c, discr_result)
-        elif result > 0:
-            x1 = (-b + (discr(*args,**kwargs) ** (0.5))) / (2 * a)
-            x2 = (-b - (discr(*args,**kwargs) ** (0.5))) / (2 * a)
-            discr_result.append(x1)
-            discr_result.append(x2)
-            return discr(a, b, c, discr_result)
         return result
+    wrapper.__name__ = function.__name__
+    wrapper.__doc__ = function.__doc__
     return wrapper
 
 @positive_result
-def discr(a:int,b:int,c:int, discr_result)->int:
-    """Считает дискриминант"""
-    return (b**2)-(4*a*c)
-# discr = lambda a, b, c : (b**2)+4*a*c
-# discr(2,2,8)
-# print (discr)
-# def kvur_withdiscr_ebanarot(discr:int)->int:
-#     """
-#     Функция принимает на вход 3 значения и считает
-#     дискриминант, если дискриминант меньше нуля,
-#     возвращает None, если ноль или больше
-#     возвращает корень/корни квадратного уравнения.
-#     """
-#
-#
-#     if discr < 0:
-#         return None
-#     elif discr == 0:
-#         x = (-b) / (2 * a)
-#         return x
-#     elif discr > 0:
-#         x1 = (-b + (D ** (0.5))) / (2 * a)
-#         x2 = (-b - (D ** (0.5))) / (2 * a)
-#         return quadratic_root
+def kvur_withdiscr_ebanarot(discr)->int:
+    """
+    Функция принимает на вход 3 значения и считает
+    дискриминант, если дискриминант меньше нуля,
+    возвращает None, если ноль или больше
+    возвращает корень/корни квадратного уравнения.
+    """
+    if discr == 0:
+        x = (-b) / (2 * a)
+        return x
+    elif discr > 0:
+        x1 = (-b + (discr ** (0.5))) / (2 * a)
+        x2 = (-b - (discr ** (0.5))) / (2 * a)
+        quadratic_root.append(x1)
+        quadratic_root.append(x2)
+        return quadratic_root
 
 
 # def kvur_graf(discr_result):
 #     plt.plot(discr_result)
 #     return plt.show()
 
-print (discr(1, -2, -24, discr_result))
+print (kvur_withdiscr_ebanarot(discr))
 # print (kvur_graf(discr_result))
 
 #HW10
